ba0a6714815a686d20b436bd9cfd0f32d17b96be
Justin Leonard <justinleonard@justins-mbp.attlocal.net>
2019-09-11 19:37:45 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,77 @@
+//global varialbe/
+var counter = 2;
+
+
+//declaring a function
+function addNumber(){
+    
+    counter = counter + 5;
+counter = counter * 2;
+counter = counter % 3;
+//I assume 0
+counter = counter % 2;
+//not what I expected 
+    console.log(counter);
+
+
+}
+
+var isEven =
+ function(number) {
+    // testing variable modulus 2 returns 0
+    if (14%2) {
+        return 0;
+        
+      } else {
+          return
+          variableNotEven;
+      }
+    
+    };
+    {
+        
+    }
+    // multiple if statements listed below
+    var answer;
+    if (2 === 2) {
+        if (2 == 2) {
+            if (2 > 0) {
+                if (0 != 2) {
+
+                }
+            }
+        }
+    }
+    
+// Exercise Loop 
+var i;
+for (i = 0; 50; i++){
+    i += 1;
+} 
+     
+   
+
+
+
+
+
+
+
+
+
+// counter = counter+5; 
+// console.log(counter);
+
+// //7
+
+// counter = counter*7;
+// console.log(counter);
+// //14
+// counter = counter % 2;
+// console.log(14%3);
+// //2
+
+// //I think the retrun will be 2
+
+// console.log(14%2);
+// //0
